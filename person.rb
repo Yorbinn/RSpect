6643e7de5daa57328f723eb8d82f92d790fd8aaa
Jorge <jorge.tll@hotmail.com>
2017-02-18 11:05:45 -0600

    Initial commit

---
+++ person.rb
@@ -0,0 +1,12 @@
+class Person
+  #Constructor
+  def initialize(name)
+  end
+
+  # Redefinicion del metodo new para enviar nil
+  # en caso de que el nombre este vacio
+  def self.new(name)
+    return nil if name.empty?
+    super
+  end
+end
